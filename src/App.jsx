b7fc0830ab0d68b0e3eb361f5721c28a780ca6da
Ankenry <dzung3511@gmail.com>
2024-02-06 23:21:42 +0700

    get start section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Value/Value";
 import Contact from "./components/Contact/Contact";
+import Getstarted from "./components/Getstarted/Getstarted";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Residencies/>
       <Value/>
       <Contact/>
+      <Getstarted/>
     </div>
   );
 }
